02933a34a7d88f5b97fa86b5c3356136356e76e0
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-11-25 17:15:02 +0300

    - Add Pod for Firebase and config in AppDelegate - Create login and signup buttons and text fields - Connect all the labels and switch and buttons - Switch the between sign up and login

--- Uber Clone/ViewController.swift
+++ Uber Clone/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var riderDriverSwitch: UISwitch!
+    
+    @IBOutlet var signUpOutlet: UIButton!
+    
+    @IBOutlet var loginOutlet: UIButton!
+    
+    @IBOutlet var riderLabel: UILabel!
+    
+    @IBOutlet var driverLabel: UILabel!
+    
+    //Variables
+    
+    var signUpMode = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if signUpMode {
+            
+            signUpOutlet.setTitle("Log In", for: .normal)
+            
+            loginOutlet.setTitle("Switch to Sign Up", for: .normal)
+            
+            riderLabel.isHidden = true
+            
+            driverLabel.isHidden = true
+            
+            riderDriverSwitch.isHidden = true
+            
+            signUpMode = false
+            
+        } else {
+            
+            signUpOutlet.setTitle("Sign Up", for: .normal)
+            
+            loginOutlet.setTitle("Switch to Log In", for: .normal)
+            
+            riderLabel.isHidden = false
+            
+            driverLabel.isHidden = false
+            
+            riderDriverSwitch.isHidden = false
+            
+            signUpMode = true
+            
+        }
+        
+    }
+    
+    
 }
 
