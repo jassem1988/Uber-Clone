496ffdd6b9614656ff9640516975eb9040fd8251
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-11-27 11:10:05 +0300

    - Import Firebase Auth and Database - Create an observer for all the added child in the database - Display the email for the child in the cell

--- Uber Clone/Controllers/DriverTableViewController.swift
+++ Uber Clone/Controllers/DriverTableViewController.swift
@@ -7,33 +7,66 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class DriverTableViewController: UITableViewController {
+    
+    //Variables
+    var rideRequests : [DataSnapshot] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Search for ride requests
+        Database.database().reference().child("RiderRequests").observe(.childAdded) { (snapshot) in
+            
+            self.rideRequests.append(snapshot)
+            
+            self.tableView.reloadData()
+            
+        }
 
-        
-        
         
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return rideRequests.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "riderRequestCell", for: indexPath)
+        
+        let snapshot = rideRequests[indexPath.row]
+        
+        if let rideRequestsDictionary = snapshot.value as? [String : AnyObject] {
+            
+            if let email = rideRequestsDictionary["email"] as? String {
+                
+                cell.textLabel?.text = email
+                
+            }
+            
+        }
+        
+        
+        
+        return cell
+        
     }
 
-    
-    
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        
+        try? Auth.auth().signOut()
+        
+        navigationController?.dismiss(animated: true, completion: nil)
+        
         
     }
     
+
 }
